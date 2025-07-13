240a5118fa462dd62a479a1913da9d27f9f56da1
ChaosCandy <chaoscandy777@gmail.com>
2025-07-13 02:50:40 -0400

    Renamed SugarStuff to SugarHud (Due to this file having more purpose than just her HP in the future) and did some cleaning up with formatting! :3 Thank you saul for getting the HP Meter to move btw!!!

---
+++ SugarHUD.lua
@@ -0,0 +1,65 @@
+local function sugarhpvaluegrab()
+    sugarhpvalue = gMarioStates[0].health >> 8
+end
+
+sugarhudhpmovevar = -32
+
+-- This is her custom power meter!
+local function sugar_hp()
+    local m = gMarioStates[0]
+
+        -- This is what handles health meter move variable, it moves the health meter up and down based on mario's health!
+    if m.playerIndex == 0 then -- Checking if you're the local player.
+       
+        if (m.health >> 8) < 8 or m.waterLevel >= m.pos.y then -- If mario's health (shifted 8 bytes) under 8, or is he underwater.
+            sugarhudhpmovevar = sugarhudhpmovevar + (8 - (sugarhudhpmovevar / 7)) -- health meter move stuff
+        else
+            if m.waterLevel < m.pos.y then
+                sugarhudhpmovevar = sugarhudhpmovevar - (5 + (sugarhudhpmovevar / 12)) -- more health meter move stuff
+            end
+        end
+
+        if sugarhudhpmovevar >= 56 then
+            sugarhudhpmovevar = 56 -- stopping the health move var from overflowing
+        end
+
+        if sugarhudhpmovevar <= -32 then
+            sugarhudhpmovevar = -32 -- stopping the health move var from underflowing
+        end
+    end
+            local SugarHp = {
+            [0] = get_texture_info("HPMeter0"),
+            [1] = get_texture_info("HPMeter1"),
+            [2] = get_texture_info("HPMeter2"),
+            [3] = get_texture_info("HPMeter3"),
+            [4] = get_texture_info("HPMeter4"),
+            [5] = get_texture_info("HPMeter5"),
+            [6] = get_texture_info("HPMeter6"),
+            [7] = get_texture_info("HPMeter7"),
+            [8] = get_texture_info("HPMeter8"),
+            }
+            
+            local hudmovetimer = 0
+            hudmovetimer = hudmovetimer + 1
+
+
+            local sugardisplayhp = math.floor(tonumber(sugarhpvalue))
+            if _G.charSelect.character_get_current_number(m.playerIndex) == CT_SUGAR and not obj_get_first_with_behavior_id(id_bhvActSelector) then
+            djui_hud_set_resolution(RESOLUTION_N64)
+            djui_hud_render_texture(SugarHp[sugardisplayhp], ((djui_hud_get_screen_width() / 2) - 32), ((djui_hud_get_screen_height() / 28) - 64) + sugarhudhpmovevar, 1, 1)
+        end
+end
+
+local function hook_update()
+    sugarhpvaluegrab()
+end
+
+
+-- The HUD Rendering In Action!
+local function on_hud_render()
+     sugar_hp()
+    end
+
+-- Hooks
+hook_event(HOOK_ON_HUD_RENDER_BEHIND, on_hud_render)
+hook_event(HOOK_UPDATE, hook_update)
